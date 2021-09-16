207e1cea7fdfeb08d59898c225e4f2343ebed2af
Tin Nguyen <venturousman@gmail.com>
2021-09-16 22:00:01 +0700

    test expo-facebook module

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello World!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import * as Facebook from "expo-facebook";
+
+// console.disableYellowBox = true;
 
 export default function App() {
-  return (
+  const [isLoggedin, setLoggedinStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isImageLoading, setImageLoadStatus] = useState(false);
+
+  const facebookLogIn = async () => {
+    try {
+      const { type, token, expires, permissions, declinedPermissions } =
+        await Facebook.logInWithReadPermissionsAsync("206059064840807", {
+          permissions: ["public_profile"],
+        });
+
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        fetch(
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,picture.height(500)`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLoggedinStatus(true);
+            setUserData(data);
+          })
+          .catch((e) => console.log(e));
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
+  const logout = () => {
+    setLoggedinStatus(false);
+    setUserData(null);
+    setImageLoadStatus(false);
+  };
+
+  return isLoggedin ? (
+    userData ? (
+      <View style={styles.container}>
+        <Image
+          style={{ width: 200, height: 200, borderRadius: 50 }}
+          source={{ uri: userData.picture.data.url }}
+          onLoadEnd={() => setImageLoadStatus(true)}
+        />
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          animating={!isImageLoading}
+          style={{ position: "absolute" }}
+        />
+        <Text style={{ fontSize: 22, marginVertical: 10 }}>
+          Hi {userData.name}!
+        </Text>
+        <TouchableOpacity style={styles.logoutBtn} onPress={logout}>
+          <Text style={{ color: "#fff" }}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    ) : null
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 50,
+          marginVertical: 20,
+        }}
+        source={require("./assets/adaptive-icon.png")}
+      />
+      <TouchableOpacity style={styles.loginBtn} onPress={facebookLogIn}>
+        <Text style={{ color: "#fff" }}>Login with Facebook</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +113,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e9ebee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginBtn: {
+    backgroundColor: "#4267b2",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  logoutBtn: {
+    backgroundColor: "grey",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    position: "absolute",
+    bottom: 0,
   },
 });
