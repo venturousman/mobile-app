9369fb7d28802002496c9e9ea07a697096cbfd03
Tin Nguyen <venturousman@gmail.com>
2021-09-18 21:00:53 +0700

    copy code from robot project that was tested to login fb successfully with username and password

--- nodejs-assets/nodejs-project/test/pageobjects/login.page.js
+++ nodejs-assets/nodejs-project/test/pageobjects/login.page.js
@@ -1,32 +1,38 @@
-import Page from './page';
+import Page from "./page";
 
 /**
  * sub page containing specific selectors and methods for a specific page
  */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get inputUsername () { return $('#username') }
-    get inputPassword () { return $('#password') }
-    get btnSubmit () { return $('button[type="submit"]') }
+  /**
+   * define selectors using getter methods
+   */
+  get inputUsername() {
+    return $("#email"); // #username
+  }
+  get inputPassword() {
+    return $("#pass"); // #password
+  }
+  get btnSubmit() {
+    return $('button[type="submit"]'); // #loginbutton
+  }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
+  /**
+   * a method to encapsule automation code to interact with the page
+   * e.g. to login using username and password
+   */
+  async login(username, password) {
+    await this.inputUsername.setValue(username);
+    await this.inputPassword.setValue(password);
+    await this.btnSubmit.click();
+  }
 
-    /**
-     * overwrite specifc options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
-    }
+  /**
+   * overwrite specifc options to adapt it to page object
+   */
+  open() {
+    return super.open("/login");
+  }
 }
 
 export default new LoginPage();
--- nodejs-assets/nodejs-project/test/specs/example.e2e.js
+++ nodejs-assets/nodejs-project/test/specs/example.e2e.js
@@ -1,15 +1,23 @@
-import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import LoginPage from "../pageobjects/login.page";
+import SecurePage from "../pageobjects/secure.page";
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
+const username = "tin_nguyen_tommy@yahoo.com";
+const password = "38919299";
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
+describe("My Login application", () => {
+  it("should login with valid credentials", async () => {
+    await LoginPage.open();
+
+    await LoginPage.login(username, password);
+    // await expect(SecurePage.flashAlert).toBeExisting();
+    // await expect(SecurePage.flashAlert).toHaveTextContaining(
+    //   "You logged into a secure area!"
+    // );
+  });
+
+  after(async () => {
+    // runs once after the last test in this block
+    const allCookies = await browser.getCookies();
+    console.log("allCookies", allCookies);
+  });
 });
-
-
